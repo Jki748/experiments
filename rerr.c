f00b9cc73f4884c45dc06395d385985c03cb191c
Jki748 <94294136+Jki748@users.noreply.github.com>
2023-12-20 18:37:15 +0300

    Create rerr.c

---
+++ rerr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+void main(void) {
+
+
+	setlocale(LC_ALL, "RUS");
+	int c, i, ch;
+	i = 0;
+	c = 0;
+
+	for (i = 0; i < 7; i++) {
+
+		ch = getchar();
+		if (ch == "x") 
+			c++;
+		if (ch == " ")
+			break;
+
+		//printf("%c\n", ch);
+
+	}
+	if (c == 0)
+		printf("нет х");
+	else
+		printf("%d\n", c);
+}
